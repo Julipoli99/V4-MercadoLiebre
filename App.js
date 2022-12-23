40fb220cb3af73372939c14c60dcb16fc955ab23
Julipoli99 <Polimenijulian9@gmail.com>
2022-12-23 15:46:06 -0300

    Creacion de servidor local con Express

---
+++ App.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const mainRouter = require("./route")
+const path = require("path");
+
+app.listen(3000, () => {
+    console.log("App running on port: 3000")
+})
+
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, "public")))
+
+
+app.use("/", mainRouter);
+
+
+
